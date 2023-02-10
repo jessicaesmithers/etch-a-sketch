e21952dc595e8b6268ea1b5df2c50ad0c332e36f
Jessica Smithers <jessicaesmithers@gmail.com>
2023-02-10 13:41:16 -0500

    Add slider to adjust grid density Add functionality to "colorful" button

--- scripts.js
+++ scripts.js
@@ -3,16 +3,25 @@ const eraser = document.querySelector("#eraser");
 const black = document.querySelector("#black");
 const colorful = document.querySelector("#colorful");
 const eraseAll = document.querySelector("#eraseAll");
-
-eraser.addEventListener("click", activateEraser);
-black.addEventListener("click", () => currentColor = "black");
-eraseAll.addEventListener("click", clearGrid);
+const slider = document.querySelector("input");
+const sliderValue = document.createElement("h3");
+const rightButtons = document.querySelector(".rightButtons");
 
 let currentColor = "black";
 
+generateGrid(55);
+ 
+sliderValue.innerHTML = slider.value + " x " + slider.value;
 
+eraser.addEventListener("click", () => currentColor = "#eeeeee");
+black.addEventListener("click", () => currentColor = "black");
+colorful.addEventListener("click", () => currentColor = "rainbow");
+eraseAll.addEventListener("click", clearGrid);
+slider.addEventListener("change", resize);
+rightButtons.appendChild(sliderValue);
 
 function generateGrid(num){
+    grid.innerHTML = "";
     for(let i = 1; i <= num * num; i++){
         let box = document.createElement("div");
         box.setAttribute("class", "box");
@@ -21,17 +30,16 @@ function generateGrid(num){
         box.style.height = (600 / num) + "px";
         box.style.backgroundColor = "#eeeeee";
         box.addEventListener("mouseover", applyColor);
+
     }
 }
 
-
-
 function applyColor(){
+    if(currentColor == "black" || currentColor == "#eeeeee"){
     this.style.backgroundColor = currentColor;
-}
-
-function activateEraser(){
-    currentColor = "#eeeeee";
+    } else {
+        this.style.backgroundColor = generateRandomColor();
+    }
 }
 
 function clearGrid(){
@@ -41,11 +49,15 @@ function clearGrid(){
     }
 }
 
+function resize(){
+    generateGrid(slider.value);
+    sliderValue.innerHTML = slider.value + " x " + slider.value;
+}
 
+function generateRandomColor(){
+    let red = Math.round(Math.random() * 255);
+    let green = Math.round(Math.random() * 255);
+    let blue = Math.round(Math.random() * 255);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
 
-
-
-
-
-
-generateGrid(90);
